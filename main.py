6ee2bed47c4787320dcdc7aabac10a7e3a5f99af
h4llow3En <Doering.Felix@googlemail.com>
2016-03-04 22:50:16 +0100

    adding catch of sigkill, hotfix cgram bug

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ __author__ = 'h4llow3En'
 import time
 import datetime
 import display
+import signal
 import imp
 import os
+import sys
 
 # custom characters
 degree_pat = (
-    0b01000,
-    0b10100,
-    0b01000,
-    0b00011,
-    0b00100,
-    0b00100,
-    0b00100,
-    0b00011)
+    0b00110,
+    0b01001,
+    0b01001,
+    0b00110,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000)
+cust_c = (
+    0b01110,
+    0b10001,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10000,
+    0b10001,
+    0b01110)
 arrow_up_pat = (
     0b00000,
     0b01111,
@@ -41,19 +52,30 @@ arrow_down_pat = (
 # sensor config
 dht = 22
 pin = 4
+lcd = None
+
 
 
 def main():
     if os.geteuid() != 0:
         exit("You need to have root privileges to run this script.\n\
         Please try again, using 'sudo'.\n\nExiting.")
+    global lcd
     lcd = display.HD44780()
+    lcd.clean()
     lcd.create_char(0, degree_pat)
-    lcd.create_char(1, arrow_up_pat)
-    lcd.create_char(2, arrow_down_pat)
+    lcd.create_char(1, cust_c)
+    lcd.create_char(2, arrow_up_pat)
+    lcd.create_char(3, arrow_down_pat)
+    lcd.clean()
     temperature.dht_init()
     run(lcd)
 
+
+def kill_all(signal, frame):
+    lcd.clean()
+    lcd.close()
+    sys.exit(0)
 
 def run(lcd):
     old_temp = 0
@@ -68,7 +90,7 @@ def run(lcd):
             temp, hum = old_temp, old_hum
 
         lcd.send_string(timestamp, row=0)
-        lcd.send_string("Temp: {} \cg:0".format(temp), row=1)
+        lcd.send_string("Temp: {} \cg:0\cg:1".format(temp), row=1)
         lcd.send_string("Humidity: {} %".format(hum), row=2)
         time.sleep(5)
 
@@ -84,6 +106,7 @@ def check_setup():
         import setup
         setup.setup()
 
+signal.signal(signal.SIGINT, kill_all)
 check_setup()
 import temperature
 
